a7e41af15f51f4cc8c909700ab0cc148be92a94e
Dolpix <31806127+kikigoldwolf@users.noreply.github.com>
2023-06-22 01:18:08 +0100

    Fix: no hardware acc support
    - Websites that spike in performance requirements will now disable there three js component, so the website runs.

--- Experience/Experience.js
+++ Experience/Experience.js
@@ -10,41 +10,72 @@ import Renderer from './Renderer'
 
 import World from './World/World.js';
 
-export default class Experience{
+export default class Experience {
     static instance;
     constructor(canvas){
-        if(Experience.instance){
-            return Experience.instance;
-        }
-        Experience.instance = this;
-        this.canvas = canvas;
-        this.scene = new THREE.Scene();
-
-        this.sizes = new Sizes();
-        this.time = new Time();
-
-        this.camera = new Camera();
-        this.renderer = new Renderer();
-
-        this.resources = new Resources(assets);
-
-        this.world = new World();
-
-        this.sizes.on("Resize", ()=>{
-            this.Resize();
-        });
-        this.time.on("Update", ()=>{
-            this.Update();
-        });
+      if(Experience.instance){
+        return Experience.instance;
+      }
+      Experience.instance = this;
+      this.canvas = canvas;
+      this.scene = new THREE.Scene();
+  
+      this.sizes = new Sizes();
+      this.time = new Time();
+  
+      this.camera = new Camera();
+      this.renderer = new Renderer();
+  
+      this.resources = new Resources(assets);
+  
+      this.world = new World();
+  
+      this.executionTimes = []; // Array to store execution times
+      this.maxExecutionTimes = 10; // Maximum size of the executionTimes array
+      this.performanceThreshold = 200; // Threshold for average performance
+  
+      this.shouldRunHeavyFunction = true;
+  
+      this.sizes.on("Resize", ()=>{
+        this.Resize();
+      });
+      this.time.on("Update", ()=>{
+        this.Update();
+      });
     }
-
+  
     Resize(){
-        this.camera.Resize();
-        this.renderer.Resize();
+      this.camera.Resize();
+      this.renderer.Resize();
     }
+  
     Update(){
-        this.camera.Update();
-        this.renderer.Update();
-        this.world.Update();
+      if (!this.shouldRunHeavyFunction) {
+        return;
+      }
+  
+      const t0 = performance.now();
+  
+      this.camera.Update();
+      this.renderer.Update();
+      this.world.Update();
+  
+      const t1 = performance.now();
+      const execTime = t1 - t0;
+  
+      // Store execution time and ensure that the executionTimes array doesn't grow indefinitely
+      this.executionTimes.push(execTime);
+      if (this.executionTimes.length > this.maxExecutionTimes) {
+        this.executionTimes.shift();
+      }
+  
+      // Compute average execution time
+      const averageExecutionTime = this.executionTimes.reduce((a, b) => a + b, 0) / this.executionTimes.length;
+  
+      if (averageExecutionTime > this.performanceThreshold) { // If average execution time is more than threshold, stop executing
+        this.shouldRunHeavyFunction = false;
+      }
+
+      //console.log(`This task took ${averageExecutionTime} milliseconds.`);
     }
 }
